d6e93931d18d5cedaabae37d6e17d8baed7fa957
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2021-01-26 22:37:58 -0500

    menu template

--- elements/layout.php
+++ elements/layout.php
@@ -10,7 +10,7 @@
 <body class="dark">
 <!--insert here the top of all page-->
 <div class="relative bg-white">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+    <div class="mx-auto px-4 sm:px-6">
         <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div class="flex justify-start">
                 <a href="/" class="inline-block mr-4">
