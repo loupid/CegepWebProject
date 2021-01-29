fb60a3ede2994f20a6e7d66f3f4604076f726ae1
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2021-01-29 11:29:51 -0500

    error page

--- views/error/error.php
+++ views/error/error.php
@@ -1,12 +1,14 @@
-<div class="flex justify-center">
-    <div class="rounded-full h-80 w-80 flex items-center opacity-75 bg-blue-600 text-white text-center text-xl font-bold">
-        <p class="w-32 ml-12">Notre site est tout cassé</p>
+<div class="flex flex-col justify-center md:flex-row">
+    <div class="flex">
+        <div class="rounded-full md:h-80 md:w-80 h-60 w-60 flex items-center opacity-75 bg-blue-600 text-white text-center text-xl font-bold">
+            <p class="w-32 ml-12">Notre site est tout cassé</p>
+        </div>
+        <div class="rounded-full md:h-80 md:w-80 h-60 w-60 flex items-center opacity-75 bg-red-600 transform md:-translate-x-32 -translate-x-24 -mr-20 content-center text-center font-bold">
+            <p class="w-20 ml-6 text-black text-3xl">Erreur 404</p>
+            <p class="w-32 ml-12 text-white text-xl">Tu n'es pas capable de taper</p>
+        </div>
     </div>
-    <div class="rounded-full h-80 w-80 flex items-center opacity-75 bg-red-600 transform -translate-x-32 content-center text-center font-bold">
-        <p class="w-20 ml-6 text-black text-3xl">Erreur 404</p>
-        <p class="w-32 ml-12 text-white text-xl">Tu n'es pas capable de taper</p>
-    </div>
-    <div class="bg-gray-800 rounded-xl h-80 mb-48 w-2/5 p-8 text-white overflow-y-auto -ml-16">
+    <div class="bg-gray-800 rounded-xl h-80 mb-48 md:w-2/5 p-8 text-white overflow-y-auto mt-8 md:ml-8 md:mt-0">
         <h3 class="text-2xl font-bold mb-4">Diagramme de Venn</h3>
         <p>
             Un diagramme de Venn (également appelé diagramme logique) est un diagramme qui montre toutes les
