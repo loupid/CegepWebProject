2c75d4bc9795cd181f3bbf11acebd4686e922c10
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2021-02-03 18:35:18 -0500

    drag and drop images

--- src/views/admin/news/newsCreate.php
+++ src/views/admin/news/newsCreate.php
@@ -19,51 +19,126 @@ dump(\controllers\Controller::breadcrum());
                         <div class="col-span-6 sm:col-span-4">
                             <label for="title" class="block text-sm font-medium text-gray-700">Titre</label>
                             <input type="text" name="title" id="title" autocomplete="title"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                   required>
                         </div>
 
                         <div class="col-span-6 sm:col-span-6 lg:col-span-3">
-                            <label for="link" class="block text-sm font-medium text-gray-700">lien/source</label>
+                            <label for="link" class="block text-sm font-medium text-gray-700">Lien/source</label>
                             <input type="text" name="link" id="link"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
+                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                   required>
                         </div>
 
                         <div class="col-span-6 sm:col-span-6">
                             <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
                             <textarea name="description" id="description" rows="8" autocomplete="description"
-                                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required></textarea>
+                                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                      required></textarea>
                         </div>
 
-                        <div class="col-span-6 sm:col-span-6">
+                        <div class="bg-white p7 rounded col-span-6 sm:col-span-6">
                             <label class="block text-sm font-medium text-gray-700">
-                                Cover photo
+                                Photo de couverture
                             </label>
-                            <div class="mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                <div class="space-y-1 text-center">
-                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                    <div class="flex text-sm text-gray-600">
-                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                            <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" class="sr-only">
-                                        </label>
-                                        <p class="pl-1">or drag and drop</p>
+                            <div x-data="dataFileDnD()" class="relative flex flex-col p-4 text-gray-400 border border-gray-200 rounded">
+                                <div x-ref="dnd"
+                                     class="relative flex flex-col text-gray-400 border border-gray-200 border-dashed rounded cursor-pointer">
+                                    <input accept="*" type="file" multiple
+                                           class="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+                                           @change="addFiles($event)"
+                                           @dragover="$refs.dnd.classList.add('border-blue-400'); $refs.dnd.classList.add('ring-4'); $refs.dnd.classList.add('ring-inset');"
+                                           @dragleave="$refs.dnd.classList.remove('border-blue-400'); $refs.dnd.classList.remove('ring-4'); $refs.dnd.classList.remove('ring-inset');"
+                                           @drop="$refs.dnd.classList.remove('border-blue-400'); $refs.dnd.classList.remove('ring-4'); $refs.dnd.classList.remove('ring-inset');"
+                                           title=""/>
+                                    <div class="mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                        <div class="space-y-1 text-center">
+                                            <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48"
+                                                 aria-hidden="true">
+                                                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                            <div class="flex text-sm text-gray-600">
+                                                <label for="file-upload"
+                                                       class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                                    <span>Téléverser un fichier</span>
+                                                </label>
+                                                <p class="pl-1"> ou glisser et déposer</p>
+                                            </div>
+                                            <p class="text-xs text-gray-500">
+                                                PNG, JPG, GIF jusqu'à 10MB
+                                            </p>
+                                        </div>
                                     </div>
-                                    <p class="text-xs text-gray-500">
-                                        PNG, JPG, GIF up to 10MB
-                                    </p>
                                 </div>
+
+                                <template x-if="files.length > 0">
+                                    <div class="grid grid-cols-2 gap-4 mt-4 md:grid-cols-6" @drop.prevent="drop($event)"
+                                         @dragover.prevent="$event.dataTransfer.dropEffect = 'move'">
+                                        <template x-for="(_, index) in Array.from({ length: files.length })">
+                                            <div class="relative flex flex-col items-center overflow-hidden text-center bg-gray-100 border rounded cursor-move select-none"
+                                                 style="padding-top: 100%;" @dragstart="dragstart($event)" @dragend="fileDragging = null"
+                                                 :class="{'border-blue-600': fileDragging == index}" draggable="true" :data-index="index">
+                                                <button class="absolute top-0 right-0 z-50 p-1 bg-white rounded-bl focus:outline-none"
+                                                        type="button" @click="remove(index)">
+                                                    <svg class="w-4 h-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                         viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                                    </svg>
+                                                </button>
+                                                <template x-if="files[index].type.includes('audio/')">
+                                                    <svg class="absolute w-12 h-12 text-gray-400 transform top-1/2 -translate-y-2/3"
+                                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                         stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                              d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"/>
+                                                    </svg>
+                                                </template>
+                                                <template x-if="files[index].type.includes('application/') || files[index].type === ''">
+                                                    <svg class="absolute w-12 h-12 text-gray-400 transform top-1/2 -translate-y-2/3"
+                                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                         stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                              d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"/>
+                                                    </svg>
+                                                </template>
+                                                <template x-if="files[index].type.includes('image/')">
+                                                    <img class="absolute inset-0 z-0 object-cover w-full h-full border-4 border-white preview"
+                                                         x-bind:src="loadFile(files[index])"/>
+                                                </template>
+                                                <template x-if="files[index].type.includes('video/')">
+                                                    <video
+                                                            class="absolute inset-0 object-cover w-full h-full border-4 border-white pointer-events-none preview">
+                                                        <fileDragging x-bind:src="loadFile(files[index])" type="video/mp4">
+                                                    </video>
+                                                </template>
+
+                                                <div class="absolute bottom-0 left-0 right-0 flex flex-col p-2 text-xs bg-white bg-opacity-50">
+                        <span class="w-full font-bold text-gray-900 truncate"
+                              x-text="files[index].name">Loading</span>
+                                                    <span class="text-xs text-gray-900" x-text="humanFileSize(files[index].size)">...</span>
+                                                </div>
+
+                                                <div class="absolute inset-0 z-40 transition-colors duration-300" @dragenter="dragenter($event)"
+                                                     @dragleave="fileDropping = null"
+                                                     :class="{'bg-blue-200 bg-opacity-80': fileDropping == index && fileDragging != index}">
+                                                </div>
+                                            </div>
+                                        </template>
+                                    </div>
+                                </template>
                             </div>
+                        </div>
+
+                    </div>
+                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button type="submit"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Publier
+                        </button>
                     </div>
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button type="submit"
-                            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Publier
-                    </button>
-                </div>
-            </div>
         </form>
     </div>
 </div>
@@ -74,4 +149,70 @@ dump(\controllers\Controller::breadcrum());
     </div>
 </div>
 
+
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+<script src="https://unpkg.com/create-file-list"></script>
+<script>
+    function dataFileDnD() {
+        return {
+            files: [],
+            fileDragging: null,
+            fileDropping: null,
+            humanFileSize(size) {
+                const i = Math.floor(Math.log(size) / Math.log(1024));
+                return (
+                    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+                    " " +
+                    ["B", "kB", "MB", "GB", "TB"][i]
+                );
+            },
+            remove(index) {
+                let files = [...this.files];
+                files.splice(index, 1);
+
+                this.files = createFileList(files);
+            },
+            drop(e) {
+                let removed, add;
+                let files = [...this.files];
+
+                removed = files.splice(this.fileDragging, 1);
+                files.splice(this.fileDropping, 0, ...removed);
+
+                this.files = createFileList(files);
+
+                this.fileDropping = null;
+                this.fileDragging = null;
+            },
+            dragenter(e) {
+                let targetElem = e.target.closest("[draggable]");
+
+                this.fileDropping = targetElem.getAttribute("data-index");
+            },
+            dragstart(e) {
+                this.fileDragging = e.target
+                    .closest("[draggable]")
+                    .getAttribute("data-index");
+                e.dataTransfer.effectAllowed = "move";
+            },
+            loadFile(file) {
+                const preview = document.querySelectorAll(".preview");
+                const blobUrl = URL.createObjectURL(file);
+
+                preview.forEach(elem => {
+                    elem.onload = () => {
+                        URL.revokeObjectURL(elem.src); // free memory
+                    };
+                });
+
+                return blobUrl;
+            },
+            addFiles(e) {
+                const files = createFileList([...this.files], [...e.target.files]);
+                this.files = files;
+                this.form.formData.files = [...files];
+            }
+        };
+    }
+</script>
 
