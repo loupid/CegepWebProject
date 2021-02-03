d61d2d77b7598e67ff099201a567e443bcc943ff
mobenab <mohammed.benabbassi@edu.cegeptr.qc.ca>
2021-02-03 17:49:34 -0500

    news

--- src/views/admin/news/newsCreate.php
+++ src/views/admin/news/newsCreate.php
@@ -23,58 +23,44 @@ dump(\controllers\Controller::breadcrum());
                         </div>
 
                         <div class="col-span-6 sm:col-span-6 lg:col-span-3">
-                            <label for="organizer" class="block text-sm font-medium text-gray-700">Organisateur</label>
-                            <input type="text" name="organizer" id="organizer"
+                            <label for="link" class="block text-sm font-medium text-gray-700">lien/source</label>
+                            <input type="text" name="link" id="link"
                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-6 lg:col-span-3">
-                            <label for="category" class="block text-sm font-medium text-gray-700">Catégorie</label>
-                            <input type="text" name="category" id="category"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="start_date" class="block text-sm font-medium text-gray-700">Début</label>
-                            <input type="datetime-local" name="start_date" id="start_date"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="end_date" class="block text-sm font-medium text-gray-700">Fin</label>
-                            <input type="datetime-local" name="end_date" id="end_date"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required>
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-4">
-                            <label for="address" class="block text-sm font-medium text-gray-700">Adresse</label>
-                            <input type="text" name="address" id="address" autocomplete="address"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-4">
-                            <label for="link" class="block text-sm font-medium text-gray-700">Lien</label>
-                            <input type="text" name="link" id="link" autocomplete="link"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         </div>
 
                         <div class="col-span-6 sm:col-span-6">
                             <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-                            <textarea name="description" id="description" rows="4" autocomplete="description"
+                            <textarea name="description" id="description" rows="8" autocomplete="description"
                                       class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" required></textarea>
                         </div>
 
-                        <div class="col-span-6 sm:col-span-3 lg:col-span-3">
-                            <label for="price" class="block text-sm font-medium text-gray-700">Prix</label>
-                            <input type="number" step="0.01" min="0" name="price" id="price" autocomplete="price"
-                                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                        </div>
+                        <div class="col-span-6 sm:col-span-6">
+                            <label class="block text-sm font-medium text-gray-700">
+                                Cover photo
+                            </label>
+                            <div class="mt-2 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                <div class="space-y-1 text-center">
+                                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    <div class="flex text-sm text-gray-600">
+                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                            <span>Upload a file</span>
+                                            <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                                        </label>
+                                        <p class="pl-1">or drag and drop</p>
+                                    </div>
+                                    <p class="text-xs text-gray-500">
+                                        PNG, JPG, GIF up to 10MB
+                                    </p>
+                                </div>
+                            </div>
                     </div>
                 </div>
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button type="submit"
                             class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Sauvegarder
+                        Publier
                     </button>
                 </div>
             </div>
@@ -87,4 +73,5 @@ dump(\controllers\Controller::breadcrum());
         <div class="border-t border-gray-200"></div>
     </div>
 </div>
+
 
