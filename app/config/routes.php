08fc797c9ecf98bdab9bbf9b41c9fb5eefab982d
BroadestOdinDev <70379540+BroadestOdinDev@users.noreply.github.com>
2021-05-26 12:17:39 -0400

    update tutoralist with bugs.

--- app/config/routes.php
+++ app/config/routes.php
@@ -60,8 +60,6 @@ $router->map('POST','/admin/created','AdminController@created', 'adminCreated');
 $router->map('POST','/admin/updated','AdminController@updated', 'adminUpdated');
 $router->map('POST','/admin/updatedprofil','AdminController@updatedProfil', 'adminUpdatedProfil');
 
-//services
-$router->map('POST', '/job', 'ServiceController@getJob', 'getJob');
 
 //events
 $router->map('GET', '/evenements', 'EventController@getAll', 'eventsIndex');
