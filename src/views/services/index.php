e0abff2298a2d3b2e0d7323e92a9e4ee5d14c183
Zachlaf <zachary.lafreniere@hotmail.ca>
2021-05-26 11:44:45 -0400

    front-end tutorat

--- src/views/services/index.php
+++ src/views/services/index.php
@@ -4,10 +4,8 @@
 <section id="tutorat">
 <div class="relative bg-white overflow-hidden">
   <div class="max-w-7xl mx-auto">
-    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-      <svg class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
-        <polygon points="50,0 100,0 50,100 0,100" />
-      </svg>
+    <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32 max-w-full">
+
 
       <div>
         <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -43,7 +41,7 @@
         </div>
       </div>
 
-      <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <main class="mt-10 mx-auto max-w-full px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex flex-row">
         <div class="sm:text-center lg:text-left">
           <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span class="block xl:inline">Le service de tutorat par les pairs</span>
@@ -57,12 +55,78 @@
           </p>
           <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div class="rounded-md shadow">
-              <a id = "inscription" href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+              <a id = "inscription" href="#formulaireinscription" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                 S'inscrire maintenant
               </a>
             </div>
           </div>
         </div>
+
+        <div class="min-w-screen bg-gray-200 flex items-center justify-center px-5 py-5 flex-col">
+
+        <div class="overflow-y-scroll p-4" style="max-height: 75vh;">
+
+    <div class="w-full m-1 mt-10 mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style="max-width: 500px">
+        <div class="w-full pt-1 pb-5">
+            
+        </div>
+        <div class="w-full mb-10">
+            <div class="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+            <p class="text-sm text-gray-600 text-center px-5">Je me suis rendu la session passée que je pouvais avoir de la difficulté avec la matière et suite à l'aide de mon tuteur la session passé j'ai réussi à passer le cours et améliorer mon niveau en programmation Java. J'ai donc continué avec le tutorat cette session et ça m'a encore plus aidé à maitriser la matière étant donné que le tuteur été toujours là pour moi et avoir des cours supplémentaires individuellement avec quelqu'un aide toujours mieux !  </p>
+            <div class="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+        </div>
+        <div class="w-full">
+            <p class="text-md text-indigo-500 font-bold text-center">Lyes Aidoun</p>
+        </div>
+    </div>
+
+
+    <div class="w-full m-1 mt-10 mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style="max-width: 500px">
+        <div class="w-full pt-1 pb-5">
+
+        </div>
+        <div class="w-full mb-10">
+            <div class="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+            <p class="text-sm text-gray-600 text-center px-5">Après avoir échoué ma première session d’introduction a la programmation, j’ai décidé de m’inscrire à ce programme-ci et cela m’a grandement aidé dans la réussite de ce cours. Le programme permet d’avoir une heure /semaine avec un étudiant de deuxième année qui t’aide dans tes laboratoires et à la compréhension des notes de cours. J’ai même décidé de continuer lors de la deuxième session et cela s’est poursuivi dans la même continuité. Je vous conseille grandement de vous inscrire car c’est bénéfique pour votre apprentissage et cela ne prend pas de temps sur vos heures libres, c’est « que » une heure / semaine c’est très clairement faisable avec les cours à côté.</p>
+            <div class="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+        </div>
+        <div class="w-full">
+            <p class="text-md text-indigo-500 font-bold text-center">Adrien Seychelles</p>
+        </div>
+    </div>
+
+    <div class="w-full m-1 mt-10 mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style="max-width: 500px">
+        <div class="w-full pt-1 pb-5">
+
+        </div>
+        <div class="w-full mb-10">
+            <div class="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+            <p class="text-sm text-gray-600 text-center px-5">Selon moi, le tutorat m'a été extrêmement utile. Le tutorat m'a aidé à comprendre la matière sous un autre point de vue. De plus, tous les tuteurs ont un âge similaire à vous, ce qui créait une meilleure relation saine entre le tuteur et l'étudiant. Depuis que je suis au cégep, tous les tuteurs que j'ai eu ont été très amicale et très aidant face aux difficultés que j'ai rencontrées dans mes programmes. Si vous avez des problèmes à propos de comprendre ou de suivre la matière de vos cours, je recommande pour toutes personnes ayant des difficultés dans leur programme de s'y inscrire.</p>
+            <div class="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+        </div>
+        <div class="w-full">
+            <p class="text-md text-indigo-500 font-bold text-center">Anthony Proulx</p>
+        </div>
+    </div>
+
+    <div class="w-full m-1 mt-10 mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800" style="max-width: 500px">
+        <div class="w-full pt-1 pb-5">
+
+        </div>
+        <div class="w-full mb-10">
+            <div class="text-3xl text-indigo-500 text-left leading-tight h-3">“</div>
+            <p class="text-sm text-gray-600 text-center px-5">Être tuteur a été pour moi une occasion d'apporter mon aide à des étudiants qui en avaient besoin. Cela m'a permis de réviser et d'approfondir certaines notions, me rendant ainsi capable de mieux les expliquer aux aidés. Ça a été un beau défi pour moi, car je devais essayer de trouver les meilleures approches pour leur expliquer les différents concepts. C'est une expérience que je recommande à ceux que ça pourrait intéresser.</p>
+            <div class="text-3xl text-indigo-500 text-right leading-tight h-3 -mt-3">”</div>
+        </div>
+        <div class="w-full">
+            <p class="text-md text-indigo-500 font-bold text-center">Raphaël Céré</p>
+        </div>
+    </div>
+
+    </div>
+
+    </div>
+
       </main>
     </div>
   </div>
@@ -119,7 +183,7 @@
     </div>
 
     <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-      <button id = "cancel" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Annuler</button>
+      <button id = "cancel" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'><a href="#tutorat">Annuler</a></button>
       <button class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Confirmer mon inscription</button>
     </div>
 
@@ -127,6 +191,133 @@
 </div>
 </div>
 </section>
+
+
+
+
+
+
+
+
+
+<section id = "job" class = "p-5">
+	<!-- component -->
+
+	<div class = "grid grid-cols-1 lg:grid-cols-2 gap-2">
+
+		<div class = "bg-white h-full text-left px-4 py-4  justify-end border-t-2 border-gray-900">
+			<a class = "flex items-center flex-wrap">
+				<img
+						class = "inline-block object-cover object-center w-24 h-24 mb-4 bg-gray-100 rounded"
+						src = "https://dummyimage.com/302x302/94a3b8/ffffff"/>
+				<span class = "flex flex-col flex-grow pl-4">
+                    <span class = "font-bold text-lg text-gray-700 -mt-4">Software developer </span>
+                    <span class = "text-md text-gray-600 uppercase font-bold">Employeur</span>
+						<span class = "text-sm text-gray-500 uppercase font-bold">819-813-1234 teste2@asdlgksalk.com</span>
+						<span class = "text-sm text-gray-400 uppercase font-bold">Salaire : 22$/h</span>
+                  </span>
+			</a>
+			<div class = "flex items-center flex-wrap ">
+				<a href = "#" onclick = "openModal('modal_job')"
+						class = "block tracking-widest uppercase text-center shadow bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">
+					consulter
+				</a>
+				<span class = " font-bold text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 px-2">
+          6 jours
+        </span>
+
+
+			</div>
+		</div>
+
+	</div>
+	<dialog id = "modal_job" class = "bg-transparent z-0 relative w-screen h-screen">
+		<div class = "p-7 flex justify-center items-center fixed left-0 top-0 w-full h-full bg-gray-900 bg-opacity-50 z-50 transition-opacity duration-300 opacity-0">
+
+				<div class = "flex flex-col items-start w-1/2">
+					<div class = "px-7 overflow-x-hidden overflow-y-auto">
+						<!-- This example requires Tailwind CSS v2.0+ -->
+						<div class = "bg-white shadow overflow-hidden sm:rounded-lg">
+
+								<div class="p-7 flex items-center w-full">
+									<div class="text-gray-900 font-bold text-lg">Nom du poste</div>
+									<svg onclick="modalClose('modal_job')" class="ml-auto fill-current text-gray-700 w-5 h-5 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+										<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z" />
+									</svg>
+								</div>
+
+
+							<div class = "border-t border-gray-200">
+								<dl>
+									<div class = "bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Employeur
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											Exemple
+										</dd>
+									</div>
+									<div class = "bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Type
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											Stage
+										</dd>
+									</div>
+									<div class = "bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Contact
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											8190010123 test@test.caom
+										</dd>
+									</div>
+
+									<div class = "bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Salaire
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											À discuter
+										</dd>
+									</div>
+									<div class = "bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Durée
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											...
+										</dd>
+									</div>
+									<div class = "bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+										<dt class = "text-sm font-medium text-gray-500">
+											Description
+										</dt>
+										<dd class = "mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+											Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt
+											cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id
+											mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur
+											mollit ad adipisicing reprehenderit deserunt qui eu.
+										</dd>
+									</div>
+								</dl>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+		</div>
+	</dialog>
+
+</section>
+
+<style>
+html{
+  scroll-behavior: smooth;
+}
+</style>
 
 <script>
 
@@ -143,4 +334,28 @@ $("#cancel").click(function(e)
 {
   $("#formulaireinscription").hide();
 })
+
+
+
+
+
+
+
+
+    function openModal(key) {
+        document.getElementById(key).showModal();
+        document.body.setAttribute('style', 'overflow: hidden;');
+        document.getElementById(key).children[0].scrollTop = 0;
+        document.getElementById(key).children[0].classList.remove('opacity-0');
+        document.getElementById(key).children[0].classList.add('opacity-100')
+    }
+
+    function modalClose(key) {
+        document.getElementById(key).children[0].classList.remove('opacity-100');
+        document.getElementById(key).children[0].classList.add('opacity-0');
+        setTimeout(function () {
+            document.getElementById(key).close();
+            document.body.removeAttribute('style');
+        }, 100);
+    }
 </script>
