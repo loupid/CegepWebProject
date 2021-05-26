f22372ccf13f5b0b8ed0a8153c3dbb8211bfa1d1
BroadestOdinDev <70379540+BroadestOdinDev@users.noreply.github.com>
2021-05-26 12:17:39 -0400

    update tutoralist with bugs.

--- src/views/admin/tutorat/tutoratlist.php
+++ src/views/admin/tutorat/tutoratlist.php
@@ -5,7 +5,7 @@ $selectedItem = ob_get_clean();
 ?>
 <h3 class="text-gray-700 text-3xl font-medium">Liste des inscriptions au tutorat</h3>
 <div class="mt-8 flex justify-end">
-<a onclick="validDelete()" class="mx-2 text-gray-200 dark:text-gray-200 hover:text-red-300 dark:hover:text-gray-400">
+<a onclick="validDeleteAll()" class="mx-2 text-gray-200 dark:text-gray-200 hover:text-red-300 dark:hover:text-gray-400">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke="red" stroke-width="2" d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke="red" stroke-width="2" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z""></path>
@@ -69,7 +69,7 @@ $selectedItem = ob_get_clean();
 
                     <!--Action Row-->
                     <td class="flex justify-center px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                        <a onclick="validDelete(<?=$etudiant->matricule?>)" class="mx-2 text-gray-200 dark:text-gray-200 hover:text-red-300 dark:hover:text-gray-400">
+                        <a onclick="validDelete(<?= $etudiant->matricule ?>)" class="mx-2 text-gray-200 dark:text-gray-200 hover:text-red-300 dark:hover:text-gray-400">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke="red" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                             </svg>
@@ -121,7 +121,7 @@ $selectedItem = ob_get_clean();
     }
 
 
-    function validDelete(){
+    function validDeleteAll(){
         swalWithBootstrapButtons.fire({
             title: 'Voulez vous vraiment supprimer la liste complete ?',
             text: "Cette action est irréversible!",
