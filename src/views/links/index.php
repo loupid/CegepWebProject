0a96bab6831b6cc02279b7a263455f9d8967ee78
Alexandra Sirois <asirois9191@gmail.com>
2021-05-26 10:17:52 -0400

    Liens utiles adaptés

--- src/views/links/index.php
+++ src/views/links/index.php
@@ -11,11 +11,11 @@
                 </div>
                 <div class="flex">
                     <a href="//<?= $link->link ?>" target="_blank"
-                       class="font-semibold text-sm text-gray-700 mr-auto hover:text-indigo-600">
+                       class="font-semibold text-sm text-gray-700 mr-auto hover:text-red-700">
                         Consulter le lien
                     </a>
                     <div class="flex ml-auto mb-1">
-                        <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg"
+                        <svg class="flex-shrink-0 h-6 w-6 text-red-700" xmlns="http://www.w3.org/2000/svg"
                              fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                   d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"></path>
